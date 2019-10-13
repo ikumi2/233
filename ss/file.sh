0774ec7568358e5251abf13bb38a78e3e96d6b67
ikumi2 <cionko@gmail.com>
2019-10-14 06:56:38 +0800

    Update file.sh

--- ss/file.sh
+++ ss/file.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-command="-a 127.0.0.1 -p 8888 -r /var/file -d /root/filebrowser.db"
-cat > /etc/systemd/system/filebrowser.service <<EOF
+command="-a 127.0.0.1 -p 8888 -r /var/file -d /root/filebrowser.db" cat > /etc/systemd/system/filebrowser.service <<EOF
 [Unit]
 Description=filebrowser
 After=network.target
